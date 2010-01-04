c3e96c140e96c6cdc9d44b72c9bd07708d5fb2f9
tstevens <tstevens@me.com>
2010-01-03 21:04:27 -0500

    Ruby formula fix. Fixes not building X86_64 when hardware is 64 bit capable. Also fixes OS included builds libiconv bug on 64 bit builds by using our own build.

--- Library/Formula/ruby.rb
+++ Library/Formula/ruby.rb
@@ -8,6 +8,12 @@ class Ruby <Formula
   @md5='9fc5941bda150ac0a33b299e1e53654c'
 
   depends_on 'readline'
+  depends_on 'libiconv'
+  
+  def patches
+    # Fixes config.guess not selcting x86_64 when running a 64 bit capable processor.
+    {:p0 => DATA}
+  end
   
   def options
     [
@@ -17,12 +23,16 @@ class Ruby <Formula
   
   def install
     ENV.gcc_4_2
+    
+    # Libiconv that comes with 10.6 has bugs when building 64bit.
+    # Using our own libiconv solves this issue
+    iconv = Formula.factory 'libiconv'
 
     args = [ "--prefix=#{prefix}",
             "--disable-debug",
             "--disable-dependency-tracking",
             "--enable-shared",
-            "--with-iconv-dir=/usr/" ]
+            "--with-iconv-dir=#{iconv.prefix}" ]
 
     args << "--program-suffix=19" if ARGV.include? "--with-suffix"
 
@@ -45,3 +55,18 @@ upgrading Ruby will lose all your gems.
     true
   end
 end
+
+__END__
+--- config.guess.orig	2010-01-02 20:51:35.000000000 -0500
++++ config.guess	2010-01-02 20:52:19.000000000 -0500
+@@ -1259,6 +1259,10 @@ EOF
+     *:Darwin:*:*)
+ 	UNAME_PROCESSOR=`uname -p` || UNAME_PROCESSOR=unknown
+ 	case $UNAME_PROCESSOR in
++	    i386) eval $set_cc_for_build
++	          if $CC_FOR_BUILD -E -dM -x c /dev/null | grep __LP64__>/dev/null 2>&1 ; then
++	            UNAME_PROCESSOR=x86_64
++	          fi ;;
+ 	    unknown) UNAME_PROCESSOR=powerpc ;;
+ 	esac
+ 	echo ${UNAME_PROCESSOR}-apple-darwin${UNAME_RELEASE}
